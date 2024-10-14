c7f780aa42cce7a235340a497d9ba638e006b8de
ReubenBaltazar <rvbaltazar@willamette.edu>
2024-10-14 08:23:14 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -18,6 +18,79 @@ def draw_image():
     gw = GWindow(WIDTH, HEIGHT)
     
     # And now it is your turn! Add your code below! Make sure you meet all the requirements!
+    body = GRect(150, 250, 100, 120)
+    body.set_color("Blue")
+    body.set_filled(True)
+    gw.add(body)
+
+    head = GOval(135, 150, 130, 130)
+    head.set_color("Orange")
+    head.set_filled(True)
+    gw.add(head)
+
+    left_eye = GOval(155, 180, 25, 25)
+    left_eye.set_color("White")
+    left_eye.set_filled(True)
+    gw.add(left_eye)
+
+    right_eye = GOval(220, 180, 25, 25)
+    right_eye.set_color("White")
+    right_eye.set_filled(True)
+    gw.add(right_eye)
+
+    left_pupil = GOval(165, 190, 8, 8)
+    left_pupil.set_color("Black")
+    left_pupil.set_filled(True)
+    gw.add(left_pupil)
+
+    right_pupil = GOval(230, 190, 8, 8)
+    right_pupil.set_color("Black")
+    right_pupil.set_filled(True)
+    gw.add(right_pupil)
+
+    mouth = GRect(175, 230, 50, 10)
+    mouth.set_color("Black")
+    mouth.set_filled(True)
+    gw.add(mouth)
+
+    # Draw the hair (a triangular shape with ovals)
+    hair_oval_1 = GOval(160, 100, 20, 60)
+    hair_oval_1.set_color("Brown")
+    hair_oval_1.set_filled(True)
+    gw.add(hair_oval_1)
+
+    hair_oval_2 = GOval(185, 70, 30, 90)
+    hair_oval_2.set_color("Brown")
+    hair_oval_2.set_filled(True)
+    gw.add(hair_oval_2)
+
+    hair_oval_3 = GOval(220, 100, 20, 60)
+    hair_oval_3.set_color("Brown")
+    hair_oval_3.set_filled(True)
+    gw.add(hair_oval_3)
+
+    left_arm = GLine(150, 280, 100, 240)  
+    gw.add(left_arm)
+
+    right_arm = GLine(250, 280, 300, 240) 
+    gw.add(right_arm)
+
+    
+    left_hand = GRect(90, 220, 20, 20)  
+    left_hand.set_color("Orange")
+    left_hand.set_filled(True)
+    gw.add(left_hand)
+
+    left_middle_finger = GLine(100, 220, 100, 200)  
+    gw.add(left_middle_finger)
+
+    right_hand = GRect(290, 220, 20, 20)  
+    right_hand.set_color("Orange")
+    right_hand.set_filled(True)
+    gw.add(right_hand)
+
+    right_middle_finger = GLine(300, 220, 300, 200)  
+    gw.add(right_middle_finger)
 
 
 
